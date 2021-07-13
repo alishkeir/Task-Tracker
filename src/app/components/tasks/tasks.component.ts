3cceed6e7b3b979bd8be07d19c661f0dde620056
Ali Shkeir <ali.shkeir@itxi.net>
2021-07-13 22:19:50 +0300

    Generated tasks, header, button, task-items and mocked tasks in a  file

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,17 @@
+import { Task } from '../../Task';
+import { Component, OnInit } from '@angular/core';
+import { TASKS } from '../../mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+  constructor() {
+    console.log(this.tasks);
+  }
+
+  ngOnInit(): void {}
+}
